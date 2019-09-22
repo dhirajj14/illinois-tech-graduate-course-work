704c6a7f26ce3607bcab0430e6abb9e4bce5c2d9
Dhiraj Jain <dhirajj14@github.com>
2019-09-22 15:13:50 -0500

    build 2 boxes graphitea and graphitemc

---
+++ Week-04/packer-vagrant-samples/scripts/post_install_graphiteb.sh
@@ -0,0 +1,77 @@
+#!/bin/bash 
+set -e
+set -v
+
+
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+# This line assumes the user you created in the preseed directory is vagrant
+# http://chrisbalmer.io/vagrant/2015/07/02/build-rhel-centos-7-vagrant-box.html
+# Read this bug track to see why this line below was the source of a lot of trouble.... 
+# https://github.com/mitchellh/vagrant/issues/1482
+#echo "Defaults requiretty" | sudo tee -a /etc/sudoers.d/init-users
+# Need to add this first as wget not part of the base package...
+sudo yum install -y wget
+#################################################################################################################
+# code needed to allow for vagrant to function seamlessly
+#################################################################################################################
+echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
+sudo groupadd admin
+sudo usermod -a -G admin vagrant
+
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+sudo mkdir -p /home/vagrant/.ssh
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+
+#########################
+# Add customization here
+#########################
+
+# Install Elrepo - The Community Enterprise Linux Repository (ELRepo)
+# https://wiki.centos.org/AdditionalResources/Repositories
+sudo yum install -y epel-release 
+
+# Install base dependencies -  Centos 7 mininal needs the EPEL repo in the line above and the package daemonize
+sudo yum update -y
+sudo yum install -y wget unzip vim git python-setuptools curl
+# Due to needing a tty to run sudo, this install command adds all the pre-reqs to build the virtualbox additions
+sudo yum install -y kernel-devel-`uname -r` gcc binutils make perl bzip2
+
+
+echo "All Done!"
+
+sudo yum install -y java-1.8.0-openjdk
+sudo yum -y install ruby ruby-devel gcc libxml2-devel
+wget https://rpmfind.net/linux/epel/6/x86_64/Packages/d/daemonize-1.7.3-1.el6.x86_64.rpm
+sudo rpm -Uvh daemonize-1.7.3-1.el6.x86_64.rpm
+
+wget https://dl.grafana.com/oss/release/grafana-6.3.5-1.x86_64.rpm
+sudo yum localinstall grafana-6.3.5-1.x86_64.rpm
+
+sudo cat << EOF >> /etc/hosts
+192.168.0.110 riemanna riemanna.example.com
+192.168.0.120 riemannb riemannb.example.com
+192.168.0.100 riemannmc riemannmc.example.com
+192.168.0.210 graphitea graphitea.example.com
+192.168.0.220 graphiteb graphiteb.example.com
+192.168.0.200 graphitemc graphitemc.example.com
+EOF
+
+sudo yum install -y epel-release
+sudo yum install -y python-setuptools
+sudo yum install -y python-whisper python-carbon
+
+sudo yum install -y python-pip gcc libffi-devel cairo-devel libtool libyaml-devel python-devel
+
+sudo pip install -U six pyparsing websocket urllib3
+sudo pip install graphite-api gunicorn
+sudo yum install grafana
+
+sudo hostnamectl set-hostname graphiteb
+
+sudo systemctl start firewalld
+sudo firewall-cmd --permanent --add-port=5000-6000/tcp
+sudo firewall-cmd --reload
+
---
+++ Week-04/packer-vagrant-samples/scripts/post_install_graphitemc.sh
@@ -0,0 +1,55 @@
+#!/bin/bash 
+set -e
+set -v
+
+
+# http://superuser.com/questions/196848/how-do-i-create-an-administrator-user-on-ubuntu
+# http://unix.stackexchange.com/questions/1416/redirecting-stdout-to-a-file-you-dont-have-write-permission-on
+# This line assumes the user you created in the preseed directory is vagrant
+echo "%admin  ALL=NOPASSWD: ALL" | sudo tee -a /etc/sudoers.d/init-users
+sudo groupadd admin
+sudo usermod -a -G admin vagrant
+
+
+# Installing vagrant keys
+wget --no-check-certificate 'https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub'
+sudo mkdir -p /home/vagrant/.ssh
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh
+cat ./vagrant.pub >> /home/vagrant/.ssh/authorized_keys
+sudo chown -R vagrant:vagrant /home/vagrant/.ssh/authorized_keys
+echo "All Done!"
+
+#http://www.fail2ban.org/wiki/index.php/MANUAL_0_8#Jails
+sudo sed -i "s/bantime = 600/bantime = -1/g" /etc/fail2ban/jail.conf
+sudo systemctl enable fail2ban
+sudo service fail2ban restart
+
+##################################################
+# Add User customizations below here
+##################################################
+
+sudo apt-get install -y openjdk-8-jre
+sudo apt-get -y install ruby ruby-dev build-essential zlib1g-dev
+
+
+sudo cat << EOF >> /etc/hosts
+192.168.0.110 riemanna riemanna.example.com
+192.168.0.120 riemannb riemannb.example.com
+192.168.0.100 riemannmc riemannmc.example.com
+192.168.0.210 graphitea graphitea.example.com
+192.168.0.220 graphiteb graphiteb.example.com
+192.168.0.200 graphitemc graphitemc.example.com
+EOF
+
+sudo apt-get update
+wget https://dl.grafana.com/oss/release/grafana_6.3.5_amd64.deb
+sudo dpkg -i grafana_6.3.5_amd64.deb
+
+sudo apt-get install -y apt-transport-https grafana
+
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install graphite-carbon
+sudo apt-get -y install graphite-api
+
+sudo hostnamectl set-hostname graphitemc
+
+ufw allow 60000:61000/tcp
