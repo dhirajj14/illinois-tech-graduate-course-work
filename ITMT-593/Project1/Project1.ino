804fa5c3767dcf06258576f1e6c68fd1b9d43350
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-02-13 14:56:19 -0600

    Implement random led generator

--- ITMT-593/Project1/Project1.ino
+++ ITMT-593/Project1/Project1.ino
@@ -1,29 +1,32 @@
-int bLedPin = 12;
-int rLedPin = 13;
-int gLedPin = 14;
 int buzzer = 16;
-
+long ran;
 void setup() {
   // put your setup code here, to run once:
 
-pinMode(bLedPin, OUTPUT);
-pinMode(rLedPin, OUTPUT);
-pinMode(gLedPin, OUTPUT);
-pinMode(buzzer, OUTPUT); 
+pinMode(12, OUTPUT); //Blue Led Pin 
+pinMode(13, OUTPUT); //Red Led Pin
+pinMode(14, OUTPUT); //Green Led Pin
+pinMode(buzzer, OUTPUT); //pin 16 - Buzzer Pin
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-digitalWrite(bLedPin, HIGH);
-tone(buzzer, 1000);
-  delay(500);
-  digitalWrite(bLedPin, LOW);
-  digitalWrite(rLedPin, HIGH);
-  tone(buzzer, 2000);
-  delay(500);
-  digitalWrite(rLedPin, LOW);
-  digitalWrite(gLedPin, HIGH);
-  tone(buzzer, 3000);
-  delay(500);
-  digitalWrite(gLedPin, LOW);
+  ran = random(12,15); //Random fuction to generate numbers between 12 to 15 randomly
+  digitalWrite(ran, HIGH); //Makes LED glow
+  tone(buzzer, 1000); //Makes Buzzer to produce tone at 1000
+  delay(500); //Delay 0f 5ms
+  digitalWrite(ran, LOW); //Makes LED off
+  delay(200); //Delay for the transition between LEDs
+  ran = random(12,15); //Random fuction to generate numbers between 12 to 15 randomly
+  digitalWrite(ran, HIGH); //Makes LED glow
+  tone(buzzer, 2000); //Makes Buzzer to produce tone at 2000
+  delay(500); //Delay 0f 5ms
+  digitalWrite(ran, LOW);
+  delay(200); //Delay for the transition between LEDs
+  ran = random(12,15); //Random fuction to generate numbers between 12 to 15 randomly
+  digitalWrite(ran, HIGH); //Makes LED glow
+  tone(buzzer, 3000); //Makes Buzzer to produce tone at 3000
+  delay(500);//Delay 0f 5ms
+  digitalWrite(ran, LOW); //Makes LED glow
+  delay(200); //Delay for the transition between LEDs
 }
