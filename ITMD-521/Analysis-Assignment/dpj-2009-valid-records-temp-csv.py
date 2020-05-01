d30f988fd892cbd1dc374d23bc0dba607addd220
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-01 17:01:02 -0500

    add col to toDF

--- ITMD-521/Analysis-Assignment/dpj-2009-valid-records-temp-csv.py
+++ ITMD-521/Analysis-Assignment/dpj-2009-valid-records-temp-csv.py
@@ -5,9 +5,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
 
-columns = ['Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code']
 
-df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF(columns)
+
+df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF('Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code')
 
 # https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame.withColumnRenamed
 dfnew = df2.filter(df2['Air_Temperature'] != 999.9)
