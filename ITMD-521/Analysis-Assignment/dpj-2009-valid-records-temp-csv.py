d57ef7ce5581984968269549e70884a8f670fe9f
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-01 13:26:10 -0500

    correct syntax error

--- ITMD-521/Analysis-Assignment/dpj-2009-valid-records-temp-csv.py
+++ ITMD-521/Analysis-Assignment/dpj-2009-valid-records-temp-csv.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
 
-columns = ['Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'AP_Quality_Code')
+columns = ['Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'AP_Quality_Code']
 
 df2 = spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF(columns)
 
