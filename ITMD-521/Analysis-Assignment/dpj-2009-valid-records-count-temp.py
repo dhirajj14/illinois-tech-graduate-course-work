d905fe75932b9e5c234113f87e93f632791b4132
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-01 14:16:57 -0500

    corrected option header to false

--- ITMD-521/Analysis-Assignment/dpj-2009-valid-records-count-temp.py
+++ ITMD-521/Analysis-Assignment/dpj-2009-valid-records-count-temp.py
@@ -11,12 +11,12 @@ spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrC
  
 columns = ['Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'AP_Quality_Code']
 
-df2 = spark.read.format("csv").option("inferSchema","true").option("header","true").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF(columns)
+df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF(columns)
 
 
 totalCount = float(df2.count())
 
-badCount = float(df2.filter(dfnew['Air_Temperature'] == 999.9).count())
+badCount = float(df2.filter(df2['Air_Temperature'] == 999.9).count())
 
 percentage = round((badCount/totalCount)*100, 2)
 
