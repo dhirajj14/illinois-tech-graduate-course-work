125ed437ad5cc80f04e02f5dfbcdc83df8b06c7b
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-01 18:27:06 -0500

    duplicate same files for atmospheric pressure with some changes

---
+++ ITMD-521/Analysis-Assignment/dpj-2009-valid-records-count-press.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import year
+from pyspark.sql.types import StringType
+from pyspark.sql.types import IntegerType
+from pyspark.sql.functions import to_date
+from pyspark.sql.types import FloatType
+
+spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
+ 
+df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF('Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code')
+
+totalCount = float(df2.count())
+
+badCount = float(df2.filter(df2['Air_Temperature'] == 9999.9).count())
+
+percentage = round((badCount/totalCount)*100, 2)
+
+dfStats = spark.createDataFrame([(totalCount,badCount,percentage)], ['Total_Record_Count', 'Bad_Record_Count','Percentage_(bad/total)*100'])
+
+print(dfStats.show())
+
+dfStats.write.format("csv").mode("overwrite").save("hdfs://namenode/output/itmd-521/dpj/dpj-2009-valid-records-count-press.csv")
+
+
---
+++ ITMD-521/Analysis-Assignment/dpj-2009-valid-records-press-csv.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+
+from pyspark.sql import SparkSession
+
+
+spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
+
+
+
+df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF('Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code')
+
+# https://spark.apache.org/docs/latest/api/python/pyspark.sql.html#pyspark.sql.DataFrame.withColumnRenamed
+dfnew = df2.filter(df2['Atmospheric_Pressure'] != 9999.9)
+
+print(dfnew.show(15))
+
+dfnew.write.format("csv").mode("overwrite").save("hdfs://namenode/output/itmd-521/dpj/dpj-2009-valid-records-press.csv")
+
