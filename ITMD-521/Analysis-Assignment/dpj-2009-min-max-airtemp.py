91f339f3dc072395dd6d5db9ba66ba14238eaafb
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-05-02 00:44:37 -0500

    corrected the typo

--- ITMD-521/Analysis-Assignment/dpj-2009-min-max-airtemp.py
+++ ITMD-521/Analysis-Assignment/dpj-2009-min-max-airtemp.py
@@ -11,7 +11,7 @@ from pyspark.sql.functions import min, max
 
 spark = SparkSession.builder.appName("Demo Spark Python Cluster Program").getOrCreate()
  
-df2 = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF('Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code')
+dfnew = spark.read.format("csv").option("inferSchema","true").option("header","false").load("hdfs://namenode/output/itmd-521/dpj/2009.csv").toDF('Weather_Station', 'WBAN', 'Observation_Date', 'Observation_Hour', 'Latitude', 'Longitude', 'Elevation', 'Wind_Direction', 'WD_Quality_Code', 'Sky_Ceiling_Height', 'SC_Quality_Code', 'Visibility_Distance', 'VD_Quality_Code', 'Air_Temperature', 'AT_Quality_Code', 'Dew_Point', 'DP_Quality_Code', 'Atmospheric_Pressure', 'AP_Quality_Code')
 
 drange = dfnew.filter(dfnew['Air_Temperature'].between(-10.0, 11.5))
 
