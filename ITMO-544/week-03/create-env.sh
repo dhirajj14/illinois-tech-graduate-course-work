8a07daa6c4421db0a68a6d08060c302848116275
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-09-17 02:10:49 -0500

    update script remove typos

--- ITMO-544/week-03/create-env.sh
+++ ITMO-544/week-03/create-env.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
 echo Installing jq package to parse instanceID
+echo \ =============================================================== \
 
 sudo apt-get install jq
 
 echo jq Installe successfully
 
-echo Creating you EC2 Instance
+echo Creating your EC2 Instance
+echo \ =============================================================== \
 
 read instanceID < <(echo $(aws ec2 run-instances --image-id ami-06b263d6ceff0b3dd --instance-type t2.micro --security-group-ids sg-9abcd0a6 --key-name windows-laptop-bionic-v2 --user-data file://install_apache.txt | jq --raw-output '.Instances[0].InstanceId'))
-reads=$(aws ec2 describe-instances --instance-ids ${instanceID} | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName')
+
+dns=$(aws ec2 describe-instances --instance-ids ${instanceID} | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName')
+
 echo Your Instance ID is ${instanceID}
+echo \ =============================================================== \
+
 echo Creating....Initializing...Starting you EC2 Instance
-while [ "$reads" == "" ]
+echo \ =============================================================== \
+
+while [ "$dns" == "" ]
 do
-    reads=$(aws ec2 describe-instances --instance-ids ${instanceID} | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName')
+    dns=$(aws ec2 describe-instances --instance-ids ${instanceID} | jq --raw-output '.Reservations[0].Instances[0].PublicDnsName')
 done
 
+
 status=$(aws ec2 describe-instances --instance-ids  ${instanceID} | jq --raw-output '.Reservations[0].Instances[0].State.Name')
+
 
 while [ "$status" != "running" ]
 do
@@ -25,8 +35,14 @@ do
     echo Initializing.....
 done
 
-echo Your Apache server is up and ready to user
+echo \ =============================================================== \
+echo Your Apache server is up and ready to use
 
-echo This is dns ${reads}
+echo \ =============================================================== \
+echo This is dns ${dns}
 
+echo \ =============================================================== \
+echo Login you into the Instance
 
+echo \ =============================================================== \
+ssh -i ./windows-laptop-bionic-v2.priv -y ubuntu@${dns}
