8915b460be22173647ac6464d98e9519480d522e
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-09-24 01:14:03 -0500

    remove load balancer error

--- ITMO-544/week-04/create-env.sh
+++ ITMO-544/week-04/create-env.sh
@@ -12,6 +12,7 @@ echo Creating your EC2 Instance
 echo \ =============================================================== \
 
 read instanceID1 instanceID2 instanceID3 < <(echo $(aws ec2 run-instances --image-id ami-06b263d6ceff0b3dd --instance-type t2.micro --security-group-ids sg-9abcd0a6 --key-name windows-laptop-bionic-v2 --user-data file://install_apache.txt --count 3 --output text --query 'Instances[*].InstanceId'))
+aws ec2 wait instance-running --instance-ids $instanceID1 $instanceID2 $instanceID3 
 
 read dns1 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID1} --output text --query 'Reservations[0].Instances[0].PublicDnsName'))
 read dns2 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID2} --output text --query 'Reservations[0].Instances[0].PublicDnsName'))
@@ -25,16 +26,18 @@ echo \ =============================================================== \
 echo Creating....Initializing...Starting you EC2 Instance
 echo \ =============================================================== \
 
-aws ec2 wait instance-running --instance-ids $instanceID1 $instanceID2 $instanceID3 
-
 read subnet1 subnet2 < <(echo $(aws ec2 describe-subnets --output text --query 'Subnets[*].SubnetId'))
 
 read vpcId < <(echo $(aws elbv2 create-load-balancer --name my-load-balancer --subnets $subnet1 $subnet2 --output text  --query 'LoadBalancers[0].VpcId'))
 
 aws elbv2 create-target-group --name my-targets --protocol HTTP --port 80 --target-type instance --vpc-id $vpcId
 
+aws elbv2 wait load-balancer-available
+
 read targetGroupArn < <(echo $(aws elbv2 describe-target-groups --output text --query TargetGroups[0].[TargetGroupArn]))
-read loadBalancerArn < <(echo $(aws elbv2 describe-target-groups --output text --query TargetGroups[0].LoadBalancerArns[0]))
+read loadBalancerArn < <(echo $(aws elbv2 describe-load-balancers --output text --query LoadBalancers[0].LoadBalancerArn))
+
+ehco load $loadBalancerArn
 
 aws elbv2 register-targets --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3
 
