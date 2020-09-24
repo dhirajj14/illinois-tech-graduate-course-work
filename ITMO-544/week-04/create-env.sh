ad99b4e6c69ea583c4e29df9a44eca68cb5d8d4d
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-09-24 03:17:15 -0500

    add destroy file

--- ITMO-544/week-04/create-env.sh
+++ ITMO-544/week-04/create-env.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo Installing jq package to parse instanceID
-echo \ =============================================================== \
-
-sudo apt-get install jq
-
-echo jq Installed successfully
-echo \ =============================================================== \
-
 echo Creating your EC2 Instance
 echo \ =============================================================== \
 
@@ -15,8 +7,8 @@ read instanceID1 instanceID2 instanceID3 < <(echo $(aws ec2 run-instances --imag
 aws ec2 wait instance-running --instance-ids $instanceID1 $instanceID2 $instanceID3 
 
 read dns1 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID1} --output text --query 'Reservations[0].Instances[0].PublicDnsName'))
-read dns2 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID2} --output text --query 'Reservations[0].Instances[0].PublicDnsName'))
-read dns3 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID3} --output text --query 'Reservations[0].Instances[0].PublicDnsName'))
+read dns2 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID2} --output text --query 'Reservations[0].Instances[1].PublicDnsName'))
+read dns3 < <(echo $(aws ec2 describe-instances --instance-ids ${instanceID3} --output text --query 'Reservations[0].Instances[2].PublicDnsName'))
 
 echo Your Instance ID is ${instanceID1}
 echo Your Instance ID is ${instanceID2}
@@ -26,23 +18,52 @@ echo \ =============================================================== \
 echo Creating....Initializing...Starting you EC2 Instance
 echo \ =============================================================== \
 
+echo Getting Subnets
+
 read subnet1 subnet2 < <(echo $(aws ec2 describe-subnets --output text --query 'Subnets[*].SubnetId'))
+
+echo Your subnets are $subnet1 and $subnet2
+
+echo \ =============================================================== \
+
+echo Getting VpcId
 
 read vpcId < <(echo $(aws elbv2 create-load-balancer --name my-load-balancer --subnets $subnet1 $subnet2 --output text  --query 'LoadBalancers[0].VpcId'))
 
+echo your VpcId is $vpcId
+
+echo \ =============================================================== \
+
+echo Getting Creating target group my-targets
+echo \ =============================================================== \
+
 aws elbv2 create-target-group --name my-targets --protocol HTTP --port 80 --target-type instance --vpc-id $vpcId
 
+echo Waiting /for load-balancer to be active
+echo \ =============================================================== \
+
 aws elbv2 wait load-balancer-available
+
+echo Getting Target Groups ARN and Load Balancer ARN
+echo \ =============================================================== \
 
 read targetGroupArn < <(echo $(aws elbv2 describe-target-groups --output text --query TargetGroups[0].[TargetGroupArn]))
 read loadBalancerArn < <(echo $(aws elbv2 describe-load-balancers --output text --query LoadBalancers[0].LoadBalancerArn))
 
-ehco load $loadBalancerArn
 
-aws elbv2 register-targets --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3
+echo Registering Target
+echo \ =============================================================== \
 
-aws elbv2 create-listener --load-balancer-arn $loadBalancerArn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$targetGroupArn
+$(aws elbv2 register-targets --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3)
 
-aws elbv2 modify-target-group --target-group-arn $targetGroupArn  --health-check-protocol HTTP --health-check-port 80
+echo Creating Listener
+echo \ =============================================================== \
+$(aws elbv2 create-listener --load-balancer-arn $loadBalancerArn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$targetGroupArn)
+
+echo Modifying Target Group
+echo \ =============================================================== \
+echo $(aws elbv2 modify-target-group --target-group-arn $targetGroupArn  --health-check-protocol HTTP --health-check-port 80)
 
 aws elbv2 describe-target-group-attributes --target-group-arn $targetGroupArn
+echo Finished!!
+echo \ =============================================================== \
--- ITMO-544/week-04/destroy-env.sh
+++ ITMO-544/week-04/destroy-env.sh
@@ -1,3 +1,20 @@
-echo enter you instanceID
-read instanceID
-aws ec2 terminate-instances --instance-ids ${instanceID}
+read instanceID1 < <(echo $(aws ec2 describe-instances --output text --query 'Reservations[0].Instances[0].InstanceId'))
+read instanceID2 < <(echo $(aws ec2 describe-instances --output text --query 'Reservations[0].Instances[1].InstanceId'))
+read instanceID3 < <(echo $(aws ec2 describe-instances --output text --query 'Reservations[0].Instances[2].InstanceId'))
+
+read targetGroupArn < <(echo $(aws elbv2 describe-target-groups --output text --query TargetGroups[0].[TargetGroupArn]))
+read loadBalancerArn < <(echo $(aws elbv2 describe-load-balancers --output text --query LoadBalancers[0].LoadBalancerArn))
+read listenerArn < <(echo $(aws elbv2 describe-listeners --load-balancer-arn $loadBalancerArn --output text --query Listeners[0].ListenerArn))
+aws elbv2 deregister-targets --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3
+
+aws elbv2 wait target-deregistered --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3,Port=80
+
+aws elbv2 delete-listener --listener-arn $listenerArn
+
+aws elbv2 delete-target-group --target-group-arn $targetGroupArn
+
+aws elbv2 delete-load-balancer --load-balancer-arn $loadBalancerArn
+
+aws elbv2 wait load-balancers-deleted --load-balancer-arns $loadBalancerArn
+
+aws ec2 terminate-instances --instance-ids $instanceID1 $instanceID2 $instanceID3
