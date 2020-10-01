60e23464bb8222ff9f1a6fc1e0643255f9df36ac
dhirajj14 <djain14@hawk.iit.edu>
2020-09-30 20:07:06 -0500

    add load blancer target commands

--- ITMO-544/week-05/create-env.sh
+++ ITMO-544/week-05/create-env.sh
@@ -17,10 +17,53 @@
 #Create launch Configuration
 
 #create-launch-configuration --launch-configuration-name <launch-configuration-name> --image-id <image-ID> --instance-type t2.micro --security-groups <Security-Group-Name --key-name <key-pair> --user-data file://install-env.sh
-create-launch-configuration --launch-configuration-name ${11} --image-id $1 --instance-type t2.micro --security-groups $6 --key-name $9 --user-data file://install_apache.sh
+aws autoscaling create-launch-configuration --launch-configuration-name ${11} --image-id $1 --instance-type t2.micro --security-groups $6 --key-name $9 --user-data file://install_apache.sh
+
+read vpcId < <(echo $(aws elbv2 create-load-balancer --name $7 --subnets $3 $4 --output text  --query 'LoadBalancers[0].VpcId'))
+
+echo your VpcId is $vpcId
+
+aws elbv2 create-target-group --name $8 --protocol HTTP --port 80 --target-type instance --vpc-id $vpcId
+
+echo Waiting /for load-balancer to be active
+echo \ =============================================================== \
+
+aws elbv2 wait load-balancer-available
+
+echo Getting Target Groups ARN and Load Balancer ARN
+echo \ =============================================================== \
+
+read targetGroupArn < <(echo $(aws elbv2 describe-target-groups --output text --query TargetGroups[0].[TargetGroupArn]))
+read loadBalancerArn < <(echo $(aws elbv2 describe-load-balancers --output text --query LoadBalancers[0].LoadBalancerArn))
+
+
+echo Registering Target
+echo \ =============================================================== \
 
 #Create auto scaling group
 
 #aws autoscaling create-auto-scaling-group --auto-scaling-group-name <auto-scalling-group-name> --launch-configuration-name <launch-configuration-name> --min-size 2 --max-size 6 --desired-capacity 3
-aws autoscaling create-auto-scaling-group --auto-scaling-group-name ${10} --launch-configuration-name ${11} --min-size 2 --max-size 6 --desired-capacity 3 --vpc-zone-identifier "$3,$4"
-   
+
+
+$(aws elbv2 register-targets --target-group-arn $targetGroupArn --targets Id=$instanceID1 Id=$instanceID2 Id=$instanceID3)
+
+aws autoscaling attach-load-balancer-target-groups --auto-scaling-group-name ${10} --target-group-arns targetGroupArn
+
+aws autoscaling attach-load-balancers --load-balancer-names $7 --auto-scaling-group-name ${10}
+
+echo Creating Listener
+echo \ =============================================================== \
+
+$(aws elbv2 create-listener --load-balancer-arn $loadBalancerArn --protocol HTTP --port 80 --default-actions Type=forward,TargetGroupArn=$targetGroupArn)
+
+echo Modifying Target Group
+echo \ =============================================================== \
+
+echo $(aws elbv2 modify-target-group --target-group-arn $targetGroupArn  --health-check-protocol HTTP --health-check-port 80)
+
+aws elbv2 describe-target-group-attributes --target-group-arn $targetGroupArn
+
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name ${10} --launch-configuration-name ${11} --min-size 2 --max-size 6 --desired-capacity $2 --vpc-zone-identifier "$3,$4"
+
+echo Finished!!
+echo \ =============================================================== \
