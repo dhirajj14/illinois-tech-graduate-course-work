a55a9e5078d6a9174ade68309797a285e81e7b72
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-11-22 18:50:25 -0600

    add python code to compress image and upload to s3

--- ITMO-544/week-10/python/retreive-message-sqs.py
+++ ITMO-544/week-10/python/retreive-message-sqs.py
@@ -5,19 +5,23 @@ from PIL import Image, ImageFilter
 
 sqs = boto3.resource('sqs',region_name='us-east-1')
 
-client = boto3.client('rds')
+client = boto3.client('rds','us-east-1')
 
-bucketNames = s3.buckets.all()
+s3 = boto3.resource('s3','us-east-1')
 
-bucketName = bucketNames[0].name
+s3Client = boto3.client('s3','us-east-1')
+
+for bucket in s3.buckets.all():
+    bucketName = bucket.name
+    break
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/sqs.html#processing-messages
 # Get the queue
 queue = sqs.get_queue_by_name(QueueName='myqueue')
 
 
 response = client.describe_db_instances(
-    DBInstanceIdentifier='string',
-    MaxRecords=1,
+    DBInstanceIdentifier='d22',
+    MaxRecords=20,
 )
 
 ENDPOINT = response.get('DBInstances')[0].get('Endpoint').get('Address')
@@ -29,7 +33,7 @@ PASSWORD = "dhirajj123"
 ### Connect to Mysql Database and retrieve record relating to the SQS job
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rds.html#RDS.Client.describe_db_instances
-# https://dev.mysql.com/doc/connector-python/en/    
+# https://dev.mysql.com/doc/connector-python/en/
 
 
 try:
@@ -44,41 +48,43 @@ mycursor = cnx.cursor()
 # Process messages by printing out body and optional author name
 for message in queue.receive_messages():
   # Print out the body and author (if set)
-  print(message.body)                 
-  mycursor.execute("SELECT * FROM jobs WHERE id == "+message.body+"")
+  print(message.body)
+  mycursor.execute("""SELECT * FROM jobs WHERE RecordNumber = %s""",(message.body,))
   myresult = mycursor.fetchall()
 
-  for x in myresult
-    fileName = urlparse(x.S3URL)
-    components = fileName.path.split["/"]
-    s3.download_file(bucketName, components[1], components[1]+'.png')
+  for x in myresult:
+    print(x)
+    fileName = urlparse(x[6])
+    components = fileName.path
+    components = components[1:]
+    s3Client.download_file(bucketName, components, components)
 
-    im = Image.open( components[1]+'.png' )
+    im = Image.open( components)
     size = (100, 100)
     im.thumbnail(size, Image.ANTIALIAS)
     background = Image.new('RGBA', size, (255, 255, 255, 0))
     background.paste(
       im, (int((size[0] - im.size[0]) / 2), int((size[1] - im.size[1]) / 2))
     )
-    background.save(components[1]+"-thumbnail.png")
+    background.save("thumbnail-"+components)
 
-    s3.Object(bucketName, components[1]+'-rendered').upload_file(components[1]+"-thumbnail.png")
+    s3.Object(bucketName, "rendered-"+components).upload_file("thumbnail-"+components)
 
-    mycursor.execute('UPDATE Jobs SET stat = 1 WHERE id ='+message.body+'')
+    mycursor.execute('UPDATE jobs SET stat = 1 WHERE id =%s',(message.body,))
 
 
 # Retreive image from the S3 bucket
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-download-file.html
 # use sample code from download-s3-bucket-image.py
 
-# Process image with PIL 
+# Process image with PIL
 # use sample code from render-image.py
 
 # Put Image Object back into S3 Bucket
 # use source code from upload-image-to-s3.py
 
  # SQL UPDATE record where id == message.body  set Stat from 0 to 1
-# https://dev.mysql.com/doc/connector-python/en/    
+# https://dev.mysql.com/doc/connector-python/en/
 
 # Let the queue know that the message is processed
-message.delete()
+  message.delete()
