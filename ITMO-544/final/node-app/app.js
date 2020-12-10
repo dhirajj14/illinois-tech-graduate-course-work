d889e3ec37358b69f66ab41a91066ab1b7bdda2a
Dhiraj Jain <54300222+dhirajj14@users.noreply.github.com>
2020-12-10 06:15:07 +0530

    update node js app

--- ITMO-544/final/node-app/app.js
+++ ITMO-544/final/node-app/app.js
@@ -102,6 +102,7 @@ promise.then((value) => {
       return 'There was an error viewing your album: ' + err.message
     }else{
       console.log(data.Contents,"<<<all content");
+      res.write("Gallery")
       data.Contents.forEach(function(obj,index){
         res.write(obj.Key + " \n")
     })
